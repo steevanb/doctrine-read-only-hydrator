7cdabe9269a1ae10f5e09c0fdd54bbaa733a76dd
Steevan BARBOYON <steevan.barboyon@gmail.com>
2016-08-17 11:03:17 +0200

    ComposerOverloadClass\steevanb\DoctrineReadOnlyHydrator\Hydrator\ReadOnlyHydrator and SimpleObjectHydrator now overload createEntity(), to call dispatchPostCreateEntityEvent()

--- ComposerOverloadClass/Hydrator/ReadOnlyHydrator.php
+++ ComposerOverloadClass/Hydrator/ReadOnlyHydrator.php
@@ -2,9 +2,28 @@
 
 namespace steevanb\DoctrineReadOnlyHydrator\Hydrator;
 
+use Doctrine\ORM\Mapping\ClassMetadata;
 use steevanb\DoctrineStats\Doctrine\ORM\Event\OverloadedHydratorTrait;
 
+/**
+ * Use it with https://github.com/steevanb/doctrine-stats
+ */
 class ReadOnlyHydrator extends \ComposerOverloadClass\steevanb\DoctrineReadOnlyHydrator\Hydrator\ReadOnlyHydrator
 {
     use OverloadedHydratorTrait;
+
+    /**
+     * @param ClassMetadata $classMetaData
+     * @param array $data
+     * @return mixed
+     * @throws \Exception
+     */
+    protected function createEntity(ClassMetadata $classMetaData, array $data)
+    {
+        $entity = parent::createEntity($classMetaData, $data);
+
+        $this->dispatchPostCreateEntityEvent($classMetaData, $data);
+
+        return $entity;
+    }
 }
